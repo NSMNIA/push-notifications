e3c1dd43da2acc3f3035864ee63cf173b966c8c4
NSMNIA <kevin.stoop@revolveddesign.com>
2022-09-20 09:57:11 +0200

    added nodemon and subscripe function
    Signed-off-by: NSMNIA <kevin.stoop@revolveddesign.com>

--- index.js
+++ index.js
@@ -6,9 +6,23 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.json());
+
 const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
 const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
 
-webPush.setVapidDetails('mailto:kevin.stoop@revolveddesign.com', publicVapidKey, privateVapidKey);
+webPush.setVapidDetails(
+    'mailto:kevin.stoop@revolveddesign.com',
+    publicVapidKey,
+    privateVapidKey
+);
 
+app.post('/subscribe', (req, res) => {
+    const subscription = req.body;
+    res.status(201).json({});
+    const payload = JSON.stringify({ title: 'Push Test' });
+    webPush.sendNotification(subscription, payload).catch(err => console.error(err));
+});
 
+app.listen(process.env.PORT || 3000, () => console.log(`Server started on port ${PORT}`));
